9a160fe969a6572e9f0e4431b11165520b21c313
David Tinianow <david.tinianow@gmail.com>
2016-09-20 11:01:54 -0600

    Create api v1 items controller and index method

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :items, only: [:index]
+    end
+  end
+
   root 'items#index'
 
   resources :items,  only: [:index, :show]
