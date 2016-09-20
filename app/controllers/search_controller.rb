4c137030381be1ac88ab17a1a33f9af60558ca9d
David Tinianow <david.tinianow@gmail.com>
2016-09-20 09:30:42 -0600

    Add faraday request to search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,12 @@
 class SearchController < ApplicationController
 
   def index
-    @stores = Store.find_all
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{params[:zipcode]},25))?format=json&apiKey=#{ENV['BEST_BUY_KEY']}&show=longName,city,distance,phone,storeType")
+    stores = JSON.parse(response.body, symbolize_names: true)
+    stores.map do |store|
+      Store.new(store)
+    end
+    # @stores = Store.find_all(stores[:stores])
   end
 
 end
--- spec/features/user_can_search_for_stores_spec.rb
+++ spec/features/user_can_search_for_stores_spec.rb
@@ -2,28 +2,30 @@ require 'rails_helper'
 
 RSpec.feature "User can search for stores" do
   scenario "they see 15 stores within 25 miles of the zipcode 80202" do
+    VCR.use_cassette('store_service_get_stores_by_zipcode') do
 
-    visit '/'
-    within('.search-field') do
-      fill_in 'zipcode', with: 80202
-      click_on 'Search'
-    end
+      visit '/'
+      within('.search-field') do
+        fill_in 'zipcode', with: 80202
+        click_on 'Search'
+      end
 
-    expect(current_path).to eq '/search'
-    expect(page).to have_content '17 Total Stores'
+      expect(current_path).to eq '/search'
+      expect(page).to have_content '17 Total Stores'
 
-    within('.results') do
-      expect(page).to have_content "Name"
-      expect(page).to have_content "City"
-      expect(page).to have_content "Distance"
-      expect(page).to have_content "Phone Number"
-      expect(page).to have_content "Store Type"
-      expect(page).to have_content "Best Buy Mobile - Cherry Creek Shopping Center"
-      expect(page).to have_content "Denver"
-      expect(page).to have_content 3.25
-      expect(page).to have_content "303-270-9189"
-      expect(page).to have_content "Mobile"
-      expect(page).to have_selector('.store', count: 15)
+      within('.results') do
+        expect(page).to have_content "Name"
+        expect(page).to have_content "City"
+        expect(page).to have_content "Distance"
+        expect(page).to have_content "Phone Number"
+        expect(page).to have_content "Store Type"
+        expect(page).to have_content "Best Buy Mobile - Cherry Creek Shopping Center"
+        expect(page).to have_content "Denver"
+        expect(page).to have_content 3.25
+        expect(page).to have_content "303-270-9189"
+        expect(page).to have_content "Mobile"
+        expect(page).to have_selector('.store', count: 15)
+      end
     end
   end
 end
