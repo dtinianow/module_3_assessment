969a3972be92ed8ae3c175207236b3fdb664d16c
David Tinianow <david.tinianow@gmail.com>
2016-09-20 09:48:31 -0600

    First user story and spec is passing"

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,7 @@ class SearchController < ApplicationController
     response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{params[:zipcode]},25))?format=json&apiKey=#{ENV['BEST_BUY_KEY']}&pageSize=15&show=longName,city,distance,phone,storeType")
     raw_stores = JSON.parse(response.body, symbolize_names: true)
     stores = raw_stores[:stores]
+    @count = raw_stores[:total]
     @stores = stores.map do |store|
       Store.new(store)
     end
