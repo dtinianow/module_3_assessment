751fa7f43acb911cf03022c5c9a3fbaa5d9ddb16
David Tinianow <david.tinianow@gmail.com>
2016-09-20 09:11:55 -0600

    Add api results to test assertion

--- spec/features/user_can_search_for_stores_spec.rb
+++ spec/features/user_can_search_for_stores_spec.rb
@@ -18,6 +18,11 @@ RSpec.feature "User can search for stores" do
       expect(page).to have_content "Distance"
       expect(page).to have_content "Phone Number"
       expect(page).to have_content "Store Type"
+      expect(page).to have_content "Best Buy Mobile - Cherry Creek Shopping Center"
+      expect(page).to have_content "Denver"
+      expect(page).to have_content 3.25
+      expect(page).to have_content "303-270-9189"
+      expect(page).to have_content "Mobile"
       expect(page).to have_selector('.store', count: 15)
     end
   end
